17cde82bac510a9d5ed907ad59e8eca067b3ddfe
cjiava251 <zlivkosyava@gmail.com>
2019-02-26 15:10:18 +0300

    save changes

--- task2.ts
+++ task2.ts
@@ -1,1 +1,19 @@
-class
+class product {
+    Name: string;
+    Quantity: number;
+    constructor(public name: string, public quantity) {
+        this.Name=name;
+        this.Quantity=quantity;
+    }
+    setQuantity(value: number) {
+        this.Quantity=value;
+    }
+    getQuantity() {
+        return this.Quantity;
+    }
+}
+
+class productMaker {
+    quantityOfProduct: number;
+    sentProduct
+}
