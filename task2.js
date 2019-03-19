0d9410f50514a103f2c03acad176f5fe776b656b
cjiava251 <zlivkosyava@gmail.com>
2019-03-19 14:30:01 +0300

    little changes. working version

--- task2.js
+++ task2.js
@@ -1,4 +1,4 @@
-class product {
+class Product {
   constructor(name, quantity) {
     this.nameOfProduct = name;
     this.quantity = quantity;
@@ -13,14 +13,14 @@ class product {
   }
 }
 
-class productMaker {
+class ProductMaker {
   constructor() {
     this.quantityOfProduct = 0;
   }
 
   makeProduct(min, range) {
     this.makedProductToday = Math.round(Math.random() * range) + min;
-    this.products = new product('Dirol', this.makedProductToday);
+    this.products = new Product('Dirol', this.makedProductToday);
     this.quantityOfProduct += this.makedProductToday;
   }
 
@@ -42,7 +42,7 @@ class productMaker {
   }
 }
 
-class consumer {
+class Consumer {
   needProduct(min, range) {
     this.needsProduct = Math.round(Math.random() * range) + min;
   }
@@ -56,49 +56,46 @@ class consumer {
   }
 }
 
-class middleMan {
+class MiddleMan {
   constructor(max) {
     this.maximumOfProducts = max;
-    //this.sentProduct = 0;
   }
 
   delivery(products, needProducts, changeProducts, changeNeeds) {
-    var c = Math.min(needProducts, products);
-    if (c > this.maximumOfProducts)
-      c = this.maximumOfProducts;
+    let c = Math.min(needProducts, products);
+    if (c > this.maximumOfProducts) c = this.maximumOfProducts;
     changeProducts.setSentProduct(c);
     changeNeeds.setRecievedProduct(c);
     this.sentProduct = c;
   }
-  
+
   getSentProduct() {
     return this.sentProduct;
   }
 }
 
-var director = new productMaker;
-var buyer = new consumer;
-var midMan = new middleMan(100);
-var makedProductOf3Days = 0, sentProductOf3Days = 0, efficiency = 0, makedProduct = [], sentProduct = [];
+const productMaker = new ProductMaker();
+const buyer = new Consumer();
+const midMan = new MiddleMan(100);
+let makedProductOf3Days = 0; let sentProductOf3Days = 0; let efficiency = 0; const makedProduct = []; const
+  sentProduct = [];
 
-console.log('Кол-во товара  Кол-во необх. товара  Кол-во доставленно товара за день  Кол-во произвед. товара за посл. 3 дня  Кол-во достав. товара за посл. 3 дня  КПД посредника ');
-for (var i = 1; i <= 10; i++) {
-  director.makeProduct(50, 100);
+function undefinedToZero(value) {
+  if (value === undefined) return 0;
+  return value;
+}
+
+console.log('Кол-во товара(в конце дня, остаток)  Кол-во необх. товара  Кол-во доставленно товара за день  Кол-во произвед. товара за посл. 3 дня  Кол-во достав. товара за посл. 3 дня  КПД посредника ');
+
+
+for (let i = 0; i <= 10; i++) {
+  productMaker.makeProduct(50, 100);
   buyer.needProduct(70, 50);
-  midMan.delivery(director.getQuantity(), buyer.getNeedsProduct(), director, buyer);
-  makedProduct[i] = director.products.getQuantity();
-  sentProduct[i] = director.getSentProduct();
-  if (i > 2) {
-    makedProductOf3Days = makedProduct[i] + makedProduct[i - 1] + makedProduct[i - 2];
-    sentProductOf3Days = sentProduct[i] + sentProduct[i - 1] + sentProduct[i - 2];
-  } else
-    if (makedProduct[2] == undefined) {
-      makedProductOf3Days = makedProduct[1];
-      sentProductOf3Days = sentProduct[1];
-    } else {
-      makedProductOf3Days = makedProduct[1] + makedProduct[2];
-      sentProductOf3Days = sentProduct[1] + sentProduct[2];
-    }
+  midMan.delivery(productMaker.getQuantity(), buyer.getNeedsProduct(), productMaker, buyer);
+  makedProduct[i] = productMaker.products.getQuantity();
+  sentProduct[i] = productMaker.getSentProduct();
+  makedProductOf3Days = makedProduct[i] + undefinedToZero(makedProduct[i - 1]) + undefinedToZero(makedProduct[i - 2]);
+  sentProductOf3Days = sentProduct[i] + undefinedToZero(sentProduct[i - 1]) + undefinedToZero(sentProduct[i - 2]);
   efficiency = midMan.getSentProduct() / 100;
-  console.log(director.getQuantity(), director.getMakedProductToday(), buyer.getNeedsProduct(), midMan.getSentProduct(), makedProductOf3Days, sentProductOf3Days, efficiency);
+  console.log(productMaker.getQuantity(), productMaker.getMakedProductToday(), buyer.getNeedsProduct(), midMan.getSentProduct(), makedProductOf3Days, sentProductOf3Days, efficiency);
 }
