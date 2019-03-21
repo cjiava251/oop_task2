bab0cd7b7ca2866344c101ef2b36ede8aa72130a
cjiava251 <zlivkosyava@gmail.com>
2019-03-21 15:01:57 +0300

    last version

--- task2.js
+++ task2.js
@@ -14,45 +14,52 @@ class Product {
 }
 
 class ProductMaker {
-  constructor() {
-    this.quantityOfProduct = 0;
+  constructor(min, max) {
+    this.quantityOfProducts = 0;
+    this.minimumProducts = min;
+    this.maximumProducts = max;
   }
 
-  makeProduct(min, range) {
-    this.makedProductToday = Math.round(Math.random() * range) + min;
-    this.products = new Product('Dirol', this.makedProductToday);
-    this.quantityOfProduct += this.makedProductToday;
+  makeProduct() {
+    this.makedProductsToday = Math.round(Math.random() * (this.maximumProducts - this.minimumProducts)) + this.minimumProducts;
+    this.products = new Product('Dirol', this.makedProductsToday);
+    this.quantityOfProducts += this.makedProductsToday;
   }
 
-  getQuantity() {
-    return this.quantityOfProduct;
+  getQuantityOfProducts() {
+    return this.quantityOfProducts;
   }
 
-  getMakedProductToday() {
-    return this.makedProductToday;
+  getMakedProductsToday() {
+    return this.makedProductsToday;
   }
 
-  setSentProduct(value) {
-    this.sentProduct = value;
-    this.quantityOfProduct -= value;
-  }
-
-  getSentProduct() {
-    return this.sentProduct;
+  giveProductsToMiddleMan(value) {
+    this.quantityOfProducts -= value;
   }
 }
 
 class Consumer {
-  needProduct(min, range) {
-    this.needsProduct = Math.round(Math.random() * range) + min;
+  constructor(min, max) {
+    this.minimumProducts = min;
+    this.maximumProducts = max;
+    this.needProducts = 0;
+  }
+  needProduct() {
+    this.needProductsToday = Math.round(Math.random() * (this.maximumProducts - this.minimumProducts)) + this.minimumProducts;
+    this.needProducts += this.needProductsToday;
   }
 
-  getNeedsProduct() {
-    return this.needsProduct;
+  getNeedProducts() {
+    return this.needProducts;
   }
 
-  setRecievedProduct(value) {
-    this.recievedProduct = value;
+  getNeedProductsToday() {
+    return this.needProductsToday;
+  }
+
+  recieveProducts(value) {
+    this.needProducts -= value;
   }
 }
 
@@ -61,41 +68,59 @@ class MiddleMan {
     this.maximumOfProducts = max;
   }
 
-  delivery(products, needProducts, changeProducts, changeNeeds) {
-    let c = Math.min(needProducts, products);
-    if (c > this.maximumOfProducts) c = this.maximumOfProducts;
-    changeProducts.setSentProduct(c);
-    changeNeeds.setRecievedProduct(c);
-    this.sentProduct = c;
+  delivery(productMaker, consumer) {
+    let min = Math.min(consumer.getNeedProducts(), productMaker.getQuantityOfProducts());
+    if (min > this.maximumOfProducts) min = this.maximumOfProducts;
+    productMaker.giveProductsToMiddleMan(min);
+    consumer.recieveProducts(min);
+    this.sentProduct = min;
   }
 
   getSentProduct() {
     return this.sentProduct;
   }
+  getMaximumOfProducts() {
+    return this.maximumOfProducts;
+  }
 }
 
-const productMaker = new ProductMaker();
-const buyer = new Consumer();
-const midMan = new MiddleMan(100);
-let makedProductOf3Days = 0; let sentProductOf3Days = 0; let efficiency = 0; const makedProduct = []; const
-  sentProduct = [];
+class Statistics {
+  constructor() {
+    this.makedProductsOf3Days = 0;
+    this.sentProductsOf3Days = 0;
+    this.efficiencyOfMiddleMan = 0;
+    this.makedProduct = [];
+    this.sentProducts = [];
+  }
 
-function undefinedToZero(value) {
-  if (value === undefined) return 0;
-  return value;
+  sumOfLastElements(array, n) {
+    let sub = array.length - n;
+    if (sub < 0) sub = 0;
+    array = array.slice(sub, array.length);
+    return array.reduce(function (sum, current) {
+      return sum + current;
+    });
+  }
+
+  startCount(days, productMaker, consumer, middleMan) {
+    for (let i = 0; i <= days; i++) {
+      productMaker.makeProduct();
+      consumer.needProduct();
+      middleMan.delivery(productMaker, consumer);
+      this.makedProduct.push(productMaker.getMakedProductsToday());
+      this.sentProducts.push(middleMan.getSentProduct());
+      this.makedProductsOf3Days = this.sumOfLastElements(this.makedProduct, 3);
+      this.sentProductsOf3Days = this.sumOfLastElements(this.sentProducts, 3);
+      this.efficiencyOfMiddleMan = middleMan.getSentProduct() / middleMan.getMaximumOfProducts();
+      console.log(productMaker.getQuantityOfProducts(), productMaker.getMakedProductsToday(), consumer.getNeedProducts(), consumer.getNeedProductsToday(), middleMan.getSentProduct(), this.makedProductsOf3Days, this.sentProductsOf3Days, this.efficiencyOfMiddleMan);
+    }
+  }
 }
 
-console.log('Кол-во товара(в конце дня, остаток)  Кол-во необх. товара  Кол-во доставленно товара за день  Кол-во произвед. товара за посл. 3 дня  Кол-во достав. товара за посл. 3 дня  КПД посредника ');
+const productMaker = new ProductMaker(50, 150);
+const consumer = new Consumer(70, 120);
+const middleMan = new MiddleMan(100);
+const stat = new Statistics();
 
-
-for (let i = 0; i <= 10; i++) {
-  productMaker.makeProduct(50, 100);
-  buyer.needProduct(70, 50);
-  midMan.delivery(productMaker.getQuantity(), buyer.getNeedsProduct(), productMaker, buyer);
-  makedProduct[i] = productMaker.products.getQuantity();
-  sentProduct[i] = productMaker.getSentProduct();
-  makedProductOf3Days = makedProduct[i] + undefinedToZero(makedProduct[i - 1]) + undefinedToZero(makedProduct[i - 2]);
-  sentProductOf3Days = sentProduct[i] + undefinedToZero(sentProduct[i - 1]) + undefinedToZero(sentProduct[i - 2]);
-  efficiency = midMan.getSentProduct() / 100;
-  console.log(productMaker.getQuantity(), productMaker.getMakedProductToday(), buyer.getNeedsProduct(), midMan.getSentProduct(), makedProductOf3Days, sentProductOf3Days, efficiency);
-}
+console.log('Кол-во товара у производителя(после доставки)  Кол-во произведенного сегодня товара Кол-во необходимого товара(после доставки)  Кол-во необходимого потребителю товара(сегодня)   Кол-во доставленно товара за день  Кол-во произвед. товара за посл. 3 дня  Кол-во достав. товара за посл. 3 дня  КПД посредника ');
+stat.startCount(10, productMaker, consumer, middleMan);
